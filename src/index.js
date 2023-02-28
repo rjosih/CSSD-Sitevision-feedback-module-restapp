e79b34b0356f7892b119a898c4d23243e7a68b70
Rosa Ekström <rosa.ekstrom@regionhalland.se>
2023-02-28 16:38:08 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import properties from '@sitevision/api/server/Properties';
+import events from '@sitevision/api/common/events';
+import resourceLocatorUtil from '@sitevision/api/server/ResourceLocatorUtil';
+import storage from '@sitevision/api/server/storage';
+const dataStore = storage.getCollectionDataStore('feedback');
+
+events.on('sv:publishing:publish', (options) => {
+
+  // Fetch the actually node object
+  const pageNode = resourceLocatorUtil.getNodeByIdentifier(options.node);
+
+  const pageId = properties.get(pageNode);
+
+  // Get the feedback posts to the pageId
+  // 100 can easily be changed to all
+  const feedbackPosts = dataStore.find(`ds.analyzed.pageId:${pageId}`, 100).toArray();
+  for (let i = 0; i < feedbackPosts.length; i++) {
+    if (feedbackPosts[i].newPost === true) {
+      const newObject = {
+        ...feedbackPosts[i],
+        newPost: false,
+      };
+        
+      try {
+        // Updated an item in the db with newPost true to false
+        dataStore.set(feedbackPosts[i].dsid, newObject)
+      } catch (e) {
+        // Error handling
+        console.log('Error: ', e)
+      }
+    }
+  }
+
+});
